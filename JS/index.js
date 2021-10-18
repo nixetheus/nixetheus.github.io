2b7544eb8479f0775752191818e33088978e72d1
nixetheus <luca.cassenti.00@gmail.com>
2021-10-18 12:09:30 +0200

    title

--- JS/index.js
+++ JS/index.js
@@ -29,11 +29,11 @@ $(document).ready(function(){
     let title;
     let text_n = 0;
     let interval = 125;
-    let titles_array = ["ASPIRING GAMEDEV", "SOFTWARE PROGRAMMER", "HUMAN"]
+    let titles_array = ["AN ASPIRING GAMEDEV", "A SOFTWARE PROGRAMMER", "HUMAN"]
 
     function title_change(n) {
 
-        n = n % titles_array.length;
+        text_n += 1;
         title = titles_array[n];
 
         wait = interval + (interval * title.length);
@@ -45,19 +45,20 @@ $(document).ready(function(){
         });
         let i = title.length;
 
-        setTimeout(function () {
-            while (i >= 0) {
-                setTimeout(function () {
-                    let text = $('#h2-hello').html();
-                    let length = text.length - 1;
-                    $('#h2-hello').html(text.substring(0, length));
-                }, wait + (interval * i));
-                i--;
-            }
-        }, 1000);
+        if (n !== titles_array.length - 1) {
+            setTimeout(function () {
+                while (i >= 0) {
+                    setTimeout(function () {
+                        let text = $('#h2-hello').html();
+                        let length = text.length - 1;
+                        $('#h2-hello').html(text.substring(0, length));
+                    }, wait + (interval * i));
+                    i--;
+                }
+            }, 1000);
 
-        text_n += 1;
-        setTimeout(function () {title_change(text_n)}, wait * 2 + 1750);
+            setTimeout(function () {title_change(text_n)}, wait * 2 + 2000);
+        }
     }
 
     title_change(text_n);
