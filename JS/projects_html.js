c49a0e4ca3d50a24b8876deb539041efd91cbea5
nixetheus <luca.cassenti.00@gmail.com>
2021-10-18 19:31:46 +0200

    about link

--- JS/projects_html.js
+++ JS/projects_html.js
@@ -39,46 +39,5 @@ let projects = [
             "</p>\n" +
             "<p class=\"project-page-proj-expl\">\n" +
             "</p>"
-    },
-
-    {
-        "name": "Cate2",
-        "code": "0000 0001",
-        "github": "https://github.com/nixetheus/CATE",
-        "desc": "CATE, the Code And Text Editor, is a side project of mine from when I was in high school.\n" +
-            "As the name implies its main use is that of an editor, particularly for python and c, since\n" +
-            "syntax highlighting and code completition is mainly available for these languages.\n" +
-            "The editor is completely written in Python using Tkinter and has a lot of customization options,\n" +
-            "particularly in the form of themes and color changes.\n" +
-            "It features a primitive internal python interpreter.\n",
-        "specs": "<p class=\"project-page-proj-expl\">\n" +
-            "Born in 2016, CATE has been my first big project since I have started programming. " +
-            "All in all, it is a simple code and text editor, with a few more complex features " +
-            "like a primitve form of syntax highlighting and code completition." +
-            "</p>\n" +
-            "<img class='project-page-proj-img' alt='cate-png' src='../Imgs/projects/cate.png'>" +
-            "<h5 class=\"subsession-title\">CUSTOMIZATION</h5>\n" +
-            "<p class=\"project-page-proj-expl\">\n" +
-            "Customization was a big during the creation of CATE, mostly because I love playing with " +
-            "colors and this was a fun way to add variety to my project" +
-            "</p>\n" +
-            "<p class=\"project-page-proj-code\">\n" +
-            "<br>" +
-            "if color == 'DARK':<br>" +
-            "&nbsp;&nbsp;colors = {<br>" +
-            "&nbsp;&nbsp;&nbsp;&nbsp;0: '#191919',  # ln_color<br>" +
-            "&nbsp;&nbsp;&nbsp;&nbsp;1: '#55FBF7',  # for_color<br>" +
-            "&nbsp;&nbsp;&nbsp;&nbsp;2: '#DEDEDE',  # text_color<br>" +
-            "&nbsp;&nbsp;&nbsp;&nbsp;3: '#242424',  # files_color<br>" +
-            "&nbsp;&nbsp;&nbsp;&nbsp;4: '#494949',  # emb_ter_col1<br>" +
-            "&nbsp;&nbsp;&nbsp;&nbsp;...<br>" +
-            "elif color == 'BLUE':<br>" +
-            "elif color == 'ICEY':<br>" +
-            "elif color == 'WHITE':<br>" +
-            "elif color == 'RANDOM':<br>" +
-            "&nbsp;&nbsp;...<br>" +
-            "</p>\n" +
-            "<p class=\"project-page-proj-expl\">\n" +
-            "</p>"
     }
 ];
